7ddc5c8bc9263f75e29524116698d33f2dd20d64
Diego <diego.ojeda@apiumhub.com>
2020-03-06 11:48:19 +0100

    Remove linkedin and wechat sdks

--- app/src/main/java/com/apiumhub/MainActivity.kt
+++ app/src/main/java/com/apiumhub/MainActivity.kt
@@ -8,110 +8,69 @@ import androidx.appcompat.app.AppCompatActivity
 import android.util.Base64
 import android.util.Log
 import com.apiumhub.social.library.*
-import com.apiumhub.social.library.LinkedinLoginManager.LinkedinConfigurationBuilder
 import com.apiumhub.social.library.SocialLoginErrorType.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 enum class SocialNetworkType {
-    FACEBOOK, GOOGLE, LINKEDIN, WECHAT
+  FACEBOOK, GOOGLE
 }
 
 class MainActivity : AppCompatActivity() {
 
-    private val facebookLoginManager = FacebookLoginManager(
-            FacebookConfiguration(listOf("public_profile", "email", "user_friends")), activity = this)
+  private val facebookLoginManager = FacebookLoginManager(
+    FacebookConfiguration(listOf("public_profile", "email", "user_friends")), activity = this
+  )
 
-    private val googleLoginManager = GoogleLoginManager(
-            GoogleConfiguration("273380565662-mop5c2flpee0ch65kjsuotl2hio88dvp.apps.googleusercontent.com", listOf("")), activity = this)
+  private val googleLoginManager = GoogleLoginManager(
+    GoogleConfiguration("273380565662-mop5c2flpee0ch65kjsuotl2hio88dvp.apps.googleusercontent.com", listOf("")), activity = this
+  )
 
-    private lateinit var linkedinLoginManager: LinkedinLoginManager
+  private lateinit var flow: SocialNetworkType
 
-    private lateinit var flow: SocialNetworkType
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        linkedinLoginManager = LinkedinConfigurationBuilder.requestId().requestEmail().build(activity = this)
-
-        activity_main_facebook_login.setOnClickListener {
-            flow = SocialNetworkType.FACEBOOK
-            facebookLoginManager.login()
-        }
-
-        activity_main_google_login.setOnClickListener {
-            flow = SocialNetworkType.GOOGLE
-            googleLoginManager.login()
-        }
-
-        activity_main_linkedin_login.setOnClickListener {
-            flow = SocialNetworkType.LINKEDIN
-            linkedinLoginManager.login()
-        }
+    activity_main_facebook_login.setOnClickListener {
+      flow = SocialNetworkType.FACEBOOK
+      facebookLoginManager.login()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when (flow) {
-            SocialNetworkType.FACEBOOK -> facebookLoginManager.onActivityResult(requestCode, resultCode, data,
-                    { userInfo: SocialUserInformation ->
-                        println(userInfo.token)
-                        println(userInfo.email)
-                    }, { error: SocialLoginException ->
-                when (error.loginError) {
-                    CANCELED -> println("CANCELED")
-                    FAILED -> println("FAILED")
-                    NO_EMAIL -> println("NO EMAIL")
-                    else -> println("OTHER ERRORS")
-                }
-            })
-            SocialNetworkType.GOOGLE -> googleLoginManager.onActivityResult(requestCode, resultCode, data,
-                    { userInfo: SocialUserInformation ->
-                        println(userInfo.token)
-                        println(userInfo.email)
-                    }) { error: SocialLoginException ->
-                when (error.loginError) {
-                    SocialLoginErrorType.CANCELED -> println("CANCELED")
-                    SocialLoginErrorType.FAILED -> println("FAILED")
-                    else -> println("OTHER ERRORS")
-                }
-            }
-            SocialNetworkType.LINKEDIN -> linkedinLoginManager.onActivityResult(requestCode, resultCode, data,
-                    { userInfo: SocialUserInformation ->
-                        println(userInfo.userId)
-                        println(userInfo.email)
-                    }) { error: SocialLoginException ->
-                when (error.loginError) {
-                    SocialLoginErrorType.CANCELED -> println("CANCELED")
-                    SocialLoginErrorType.FAILED -> println("FAILED ${error.originalError.toString()}")
-                    else -> println("OTHER ERRORS")
-                }
-            }
-        }
-
+    activity_main_google_login.setOnClickListener {
+      flow = SocialNetworkType.GOOGLE
+      googleLoginManager.login()
     }
+  }
 
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
 
-    private fun getPackageHash() {
-        try {
-
-            @SuppressLint("PackageManagerGetSignatures") val info = packageManager.getPackageInfo(
-                    "com.apiumhub.social", //give your package name here
-                    PackageManager.GET_SIGNATURES)
-            for (signature in info.signatures) {
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-
-                Log.d("linkedin",
-                        "Hash  : " + Base64.encodeToString(md.digest(), Base64.NO_WRAP))//Key hash is printing in Log
-            }
-        } catch (e: PackageManager.NameNotFoundException) {
-            Log.d("linkedin", e.message, e)
-        } catch (e: NoSuchAlgorithmException) {
-            Log.d("linkedin", e.message, e)
+    when (flow) {
+      SocialNetworkType.FACEBOOK -> facebookLoginManager.onActivityResult(requestCode, resultCode, data,
+        { userInfo: SocialUserInformation ->
+          println(userInfo.token)
+          println(userInfo.email)
+        }, { error: SocialLoginException ->
+          when (error.loginError) {
+            CANCELED -> println("CANCELED")
+            FAILED -> println("FAILED")
+            NO_EMAIL -> println("NO EMAIL")
+            else -> println("OTHER ERRORS")
+          }
+        })
+      SocialNetworkType.GOOGLE -> googleLoginManager.onActivityResult(requestCode, resultCode, data,
+        { userInfo: SocialUserInformation ->
+          println(userInfo.token)
+          println(userInfo.email)
+        }) { error: SocialLoginException ->
+        when (error.loginError) {
+          SocialLoginErrorType.CANCELED -> println("CANCELED")
+          SocialLoginErrorType.FAILED -> println("FAILED")
+          else -> println("OTHER ERRORS")
         }
-
+      }
     }
+  }
 }
